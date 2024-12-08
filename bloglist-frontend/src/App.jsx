107348bd7fd99ac2564b7f7b6eb64aad9a62a419
Lothrin <m.erkam.arafat@gmail.com>
2024-12-08 13:30:13 +0100

    5.17: Blog List End To End Testing, step 1

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -100,6 +100,7 @@ const App = () => {
           value={username}
           name="Username"
           onChange={({ target }) => setUsername(target.value)}
+          data-testid="username"
         />
       </div>
       <div>
@@ -109,6 +110,7 @@ const App = () => {
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
+          data-testid="password"
         />
       </div>
       <button type="submit">login</button>

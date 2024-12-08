d46e2656e9d933859b7c0deeae6ef022ca194df3
Lothrin <m.erkam.arafat@gmail.com>
2024-12-08 14:30:16 +0100

    5.18: Blog List End To End Testing, step 2
    Additional Changes:
    -test env config
    -routing for test env database reset

--- _E2E_Blog/tests/blog_app.spec.js
+++ _E2E_Blog/tests/blog_app.spec.js
@@ -1,12 +1,40 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
+
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Erkam Arafat',
+        username: 'root',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'root', 'salainen')
+      await expect(page.getByText('Erkam Arafat logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'wrongname', 'wrongpassword')
+
+    const errorDiv = await page.locator('.error')
+    await expect(errorDiv).toContainText('Wrong Credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    await expect(page.getByText('Erkam Arafat')).not.toBeVisible()
+    })
   })
 })
